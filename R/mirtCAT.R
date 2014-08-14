b050f1942e81dc4575e54ca3f7bc4effcf96fcaf
Phil Chalmers <rphilip.chalmers@gmail.com>
2014-08-14 12:36:15 -0400

    rm start_time

--- R/mirtCAT.R
+++ R/mirtCAT.R
@@ -348,7 +348,7 @@ mirtCAT <- function(questions = NULL, mirt_object = NULL, method = 'MAP', criter
         colnames(ret$thetas_SE_history) <- paste0('Theta_', 1L:MCE$test$nfact)
     if(!person$score)
         ret$thetas <- ret$thetas_history <- ret$thetas_SE_history <- NA
-    MCE$person <- MCE$test <- MCE$design <- MCE$shinyGUI <- 
+    MCE$person <- MCE$test <- MCE$design <- MCE$shinyGUI <- MCE$start_time <- 
         MCE$STOP <- MCE$outfile <- NULL
     class(ret) <- 'mirtCAT'
     ret
