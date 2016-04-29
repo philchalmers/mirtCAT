b7f5b5a77b0d78579fb5c05ef919cce3337bc4f0
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-04-29 16:58:51 -0400

    start item fix

--- R/selection_criteria.R
+++ R/selection_criteria.R
@@ -40,9 +40,11 @@ MLWI <- function(which_not_answered, person, test, thetas, prior = FALSE){
     pick1 <- na.omit(person$items_answered)
     pars <- test@mo@ParObjects$pars[c(pick1, test@length + 1)]
     itemloc <- c(0, cumsum(test@mo@Data$K[pick1])) + 1L
-    ll <- log(mirt:::computeItemtrace(pars=pars, Theta=Theta, itemloc = itemloc, CUSTOM.IND=list()))
-    pick2 <- itemloc[-length(itemloc)] + person$responses[pick1]
-    LL <- rowSums(ll[ ,pick2, drop=FALSE])
+    if(length(pick1)){
+        ll <- log(mirt:::computeItemtrace(pars=pars, Theta=Theta, itemloc = itemloc, CUSTOM.IND=list()))
+        pick2 <- itemloc[-length(itemloc)] + person$responses[pick1]
+        LL <- rowSums(ll[ ,pick2, drop=FALSE])
+    } else LL <- rep(1, nrow(Theta))
     Is <- matrix(NA, nrow(Theta), length(which_not_answered))
     for(i in 1L:nrow(Theta))
         Is[i, ] <- .Call('ComputeCriteria', test@EIs, Theta[i, ,drop=FALSE], which_not_answered, 
