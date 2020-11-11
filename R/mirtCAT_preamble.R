a180431b65e65577bebeb93bb223d7874d84d10d
philchalmers <rphilip.chalmers@gmail.com>
2020-11-11 15:56:26 -0500

    fixes #32

--- R/mirtCAT_preamble.R
+++ R/mirtCAT_preamble.R
@@ -98,8 +98,7 @@ mirtCAT_preamble_internal <-
                 for(pick in which(df$HTMLOptions)){
                     tmpopts <- lapply(na.omit(as.character(Options[pick, ])), HTML)
                     names(tmpopts) <- NULL
-                    shinyGUI$choiceNames[[pick]] <- tmpopts
-                    shinyGUI$choiceValues[[pick]] <- as.list(colnames(Options)[1L:length(tmpopts)])
+                    shinyGUI$choiceValues[[pick]] <- shinyGUI$choiceNames[[pick]] <- tmpopts
                 }
                 df$HTMLOptions <- NULL
             }
